fe13e89da7586ecbd00433e223f4718457973ada
thol201 <131551405+thol201@users.noreply.github.com>
2023-04-22 20:33:01 +0200

    Add files via upload

---
+++ McCSharp/McCSharp/Figure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Figure
+    {
+        public byte code;
+        public int size;
+        public bool falling;
+        public int x;
+        public int y;
+        public Figure() { x = 0;y = 0; tab = null;rotation = 0; falling = true; }
+        public byte[,]? tab;
+        public int rotation;
+        public virtual void rotate(int z)
+        {
+            byte[,] tmp = new byte[size, size];
+            for (int i = 0; i < size; i++)
+                for (int k = 0; k < size; k++)
+                    if(z== 0)
+                       tmp[size - i - 1, k] = tab[k, i];
+                    else
+                        tmp[k, i] = tab[size - i - 1, k];
+
+            tab = tmp;
+        }
+        public virtual void change_code() 
+        {
+            for (int i = 0; i < tab.GetLength(0); i++)
+                for (int k = 0; k < tab.GetLength(1); k++)
+                    if (tab[i,k]!=0)
+                        tab[i,k] = code;
+                    }
+    }
+}
---
+++ McCSharp/McCSharp/Map_Printer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Map_Printer
+    {
+        Map mapptr = null;
+        public Map_Printer() { }
+        public Map_Printer(Map k) { mapptr= k;}
+
+        public void print()
+        {
+            for (int i = 0; i < mapptr.ptr.GetLength(0); i++)
+            {
+                for (int k = 0; k < mapptr.ptr.GetLength(1); k++)
+                {
+                    if(mapptr.ptr[i, k]!=0)
+                        if (mapptr.ptr[i,k]>100)
+                            Console.Write(mapptr.ptr[i, k]);
+                        else
+                            Console.Write(" "+mapptr.ptr[i, k] + " ");
+
+                    else
+                        Console.Write(" _ ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
